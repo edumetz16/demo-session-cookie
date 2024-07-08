5c21784c56bda98e8a03d9acdbf450e8e3cb67e7
edumetz16 <eduardo@linead.com.ar>
2024-07-08 18:22:43 -0300

    show session cookie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Image from "next/image";
 import Button from "./button";
 
 export default function Home() {
-console.log(cookies().getAll());
+
+  const sessionCookie = cookies().get("__session");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>{`Session cookie from server ${sessionCookie}`}</p>
       <Button />
     </main>
   );
